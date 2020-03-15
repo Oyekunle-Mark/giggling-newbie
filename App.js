ae7c9b001cf09a702337cb6f4de5cc54f1ccef44
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-15 20:35:51 +0100

    Created a center text

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ const App = () => (
   <SafeAreaView>
     <View style={styles.container}>
       <Text style={styles.text}>Here are some boxes of different colors</Text>
+      <View style={styles.cyanBox}>
+        <Text style={styles.boxText}>Cyan Box: #2aa198</Text>
+      </View>
     </View>
   </SafeAreaView>
 );
@@ -16,6 +19,16 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 18,
+    fontWeight: 'bold',
+  },
+  cyanBox: {
+    backgroundColor: '#2aa198',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  boxText: {
+    color: 'white',
     fontWeight: 'bold',
   },
 });
