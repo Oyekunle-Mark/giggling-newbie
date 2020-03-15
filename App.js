5b35890ce9a3f2efacbd4a372ebb00b9c7da16b1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-15 20:24:03 +0100

    Removed the styles

--- App.js
+++ App.js
@@ -2,23 +2,13 @@ import React from 'react';
 import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 
 const App = () => (
-  <SafeAreaView style={styles.screen}>
-    <View style={styles.container}>
+  <SafeAreaView>
+    <View>
       <Text>Hello, world!</Text>
     </View>
   </SafeAreaView>
 );
 
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
-  container: {
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
