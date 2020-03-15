e441255cac8f39e7cc155ca48145eeee0e959c82
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-15 20:29:03 +0100

    Added padding to the view container

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 
 const App = () => (
   <SafeAreaView>
-    <View>
+    <View style={styles.container}>
       <Text style={styles.text}>Here are some boxes of different colors</Text>
     </View>
   </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    paddingTop: 30,
+  },
   text: {
     fontSize: 18,
     fontWeight: 'bold',
