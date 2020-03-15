50dfcd5b97ec4f42b1e01bb1dad730cf47601f33
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-15 21:19:41 +0100

    Reused styles as props

--- components/ColorBox.js
+++ components/ColorBox.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
-export default () => (
-  <View>
-    <Text>I am a Color Box</Text>
-  </View>
-);
+export default ({colorName, hexCode}) => {
+  const boxColor = {
+    backgroundColor: hexCode,
+  };
+
+  return (
+    <View style={[styles.box, boxColor]}>
+      <Text style={styles.boxText}>
+        {colorName}: {hexCode}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  box: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 5,
+    borderRadius: 5,
+  },
+  boxText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
