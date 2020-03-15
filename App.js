83ed79d2c88a4aff9e36ba3159d17ebbe2e69804
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-15 20:12:26 +0100

    Added a style to the view component

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 
 const App = () => (
   <SafeAreaView>
-    <View>
+    <View styles={styles}>
       <Text>Hello, world!</Text>
     </View>
   </SafeAreaView>
 );
+
+const styles = StyleSheet.create({
+  backgroundColor: 'teal',
+});
 
 export default App;
