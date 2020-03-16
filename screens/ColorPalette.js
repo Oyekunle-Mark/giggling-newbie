55ca3e7434a946b057e7a5cce1dbf4d5029b20f8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-16 21:14:24 +0100

    Changed the background color to white

--- screens/ColorPalette.js
+++ screens/ColorPalette.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import { View, Text, FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 import ColorBox from '../components/ColorBox';
 import { COLORS } from '../data';
 
 export default () => (
-  <SafeAreaView>
-    <View style={styles.container}>
-      <FlatList
-        data={COLORS}
-        keyExtractor={item => item.hexCode}
-        renderItem={({ item }) => (
-          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
-        )}
-        ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
-      />
-    </View>
-  </SafeAreaView>
+  <View style={styles.container}>
+    <FlatList
+      data={COLORS}
+      keyExtractor={item => item.hexCode}
+      renderItem={({ item }) => (
+        <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+      )}
+      ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
+    />
+  </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 10,
-    paddingTop: 30,
+    paddingTop: 10,
+    backgroundColor: 'white',
   },
   text: {
     fontSize: 18,
