8dcb4f68b2080e507940ead4e865ac102f757b1e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-16 21:23:59 +0100

    Passed the colors and name as params

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
+import { COLORS } from '../data';
+
 export default ({ navigation }) => (
   <View>
     <TouchableOpacity
       onPress={() => {
-        navigation.navigate('ColorPalette');
+        navigation.navigate('ColorPalette', {
+          paletteName: 'Solarized',
+          colors: COLORS,
+        });
       }}>
       <Text>Solarized</Text>
     </TouchableOpacity>
