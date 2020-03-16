61ebac6b8de13fda6faff088a322d830618de572
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-16 22:15:33 +0100

    Displays five colors

--- components/PalettePreview.js
+++ components/PalettePreview.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 
 export default ({ handlePress, palette }) => (
   <TouchableOpacity onPress={handlePress}>
     <Text style={styles.text}>{palette.paletteName}</Text>
+    <FlatList
+      horizontal={true}
+      data={palette.colors.slice(0, 5)}
+      keyExtractor={item => item.colorName}
+      renderItem={({ item }) => <Text>{item.colorName}</Text>}
+    />
   </TouchableOpacity>
 );
 
