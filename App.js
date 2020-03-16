548c0dbd979c0dafa1ca3c3c82d36659cd23d865
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-16 21:00:26 +0100

    Removed the color pallete code

--- App.js
+++ App.js
@@ -1,38 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-import ColorBox from './components/ColorBox';
-import { COLORS } from './data';
-
-const App = () => (
-  <NavigationContainer>
-    <SafeAreaView>
-      <View style={styles.container}>
-        <FlatList
-          data={COLORS}
-          keyExtractor={item => item.hexCode}
-          renderItem={({ item }) => (
-            <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
-          )}
-          ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
-        />
-      </View>
-    </SafeAreaView>
-  </NavigationContainer>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 10,
-    paddingTop: 30,
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-});
+const App = () => <NavigationContainer></NavigationContainer>;
 
 export default App;
