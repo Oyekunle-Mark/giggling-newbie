ce41c8ea22530380c1d507580a049906d72956bf
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-16 21:27:34 +0100

    Passed the colot as props

--- screens/ColorPalette.js
+++ screens/ColorPalette.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 import ColorBox from '../components/ColorBox';
-import { COLORS } from '../data';
 
-export default () => (
-  <View style={styles.container}>
-    <FlatList
-      data={COLORS}
-      keyExtractor={item => item.hexCode}
-      renderItem={({ item }) => (
-        <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
-      )}
-      ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
-    />
-  </View>
-);
+export default ({ route }) => {
+  const { paletteName, colors } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={colors}
+        keyExtractor={item => item.hexCode}
+        renderItem={({ item }) => (
+          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+        )}
+        ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
