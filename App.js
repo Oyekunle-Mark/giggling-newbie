6907b5fa50c3a6c5c75b817d71007c8c22f73bd6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-16 21:01:41 +0100

    Carried out imports

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './screens/Home';
+import ColorPallete from './screens/ColorPallete';
 
 const App = () => <NavigationContainer></NavigationContainer>;
 
