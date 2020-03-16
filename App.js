d298414a9bcdaff2f634ba99cac69fd45893977b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-16 20:47:49 +0100

    Used the react native navigation

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 import ColorBox from './components/ColorBox';
 import { COLORS } from './data';
 
 const App = () => (
-  <SafeAreaView>
-    <View style={styles.container}>
-      <FlatList
-        data={COLORS}
-        keyExtractor={item => item.hexCode}
-        renderItem={({ item }) => (
-          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
-        )}
-        ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
-      />
-    </View>
-  </SafeAreaView>
+  <NavigationContainer>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <FlatList
+          data={COLORS}
+          keyExtractor={item => item.hexCode}
+          renderItem={({ item }) => (
+            <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+          )}
+          ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
+        />
+      </View>
+    </SafeAreaView>
+  </NavigationContainer>
 );
 
 const styles = StyleSheet.create({
