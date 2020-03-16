72885f7e6af18de12b7d2321b3d08bd74766b4ba
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-16 22:01:21 +0100

    passes the onclick as props

--- components/PalettePreview.js
+++ components/PalettePreview.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-export default () => (
-  <TouchableOpacity
-    onPress={() => {
-      navigation.navigate('ColorPalette', item);
-    }}>
-    <Text>{item.paletteName}</Text>
+export default ({ handlePress, palette }) => (
+  <TouchableOpacity onPress={handlePress}>
+    <Text>{palette.paletteName}</Text>
   </TouchableOpacity>
 );
