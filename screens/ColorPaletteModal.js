f5117c35f4018253dd13076ea3596ce45c0e3379
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-17 20:00:18 +0100

    Styled the button

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
 export default () => {
   const [name, setName] = useState('');
@@ -7,21 +13,35 @@ export default () => {
   return (
     <View style={styles.container}>
       <TextInput style={styles.input} value={name} onChangeText={setName} />
-      <Text>This is a Modal!</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   input: {
-    backgroundColor: 'gray',
+    backgroundColor: 'whitesmoke',
     borderWidth: 1,
     padding: 10,
     borderRadius: 5,
+    marginBottom: 50,
   },
   container: {
     padding: 5,
     backgroundColor: 'white',
     flex: 1,
+  },
+  button: {
+    height: 40,
+    backgroundColor: 'teal',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
