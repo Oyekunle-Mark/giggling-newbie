aacfd49a05297c4a8c2ca1183dd0d8762cbed249
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-17 19:53:53 +0100

    Styled the input

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 export default () => {
   const [name, setName] = useState('');
 
   return (
-    <View>
-      <TextInput value={name} onChangeText={setName} />
+    <View style={styles.container}>
+      <TextInput style={styles.input} value={name} onChangeText={setName} />
       <Text>This is a Modal!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+  },
+  container: {
+    padding: 5,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+});
